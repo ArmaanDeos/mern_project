356aaf6026b4485038964b11ee1ddb203fae9cad
TechBingeWorld <ahmad.arman.deos@gmail.com>
2024-03-20 00:57:56 +0530

    adding login functionality and storing data into localstrorage

---
+++ admin/dashboard/src/test.js
@@ -0,0 +1,7 @@
+{
+    "user": "{\"currentUser\":null,\"isFetching\":false,\"error\":false}",
+    "currentUser": "{\"statusCode\":200,\"data\":{\"user\":{\"_id\":\"65bd11ecbda1a27a664f51ff\",\"username\":\"admin\",\"email\":\"admin@gmail.com\",\"isAdmin\":true,\"createdAt\":\"2024-02-02T16:01:48.519Z\",\"updatedAt\":\"2024-02-02T16:01:48.519Z\",\"__v\":0},\"accessToken\":\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YmQxMWVjYmRhMWEyN2E2NjRmNTFmZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMDg2NDk3MCwiZXhwIjoxNzEwOTUxMzcwfQ.lsAiIFSRKKNGUI_5wlyPZ3hVnIN8Luwt-q5u8h-Yl8E\"},\"message\":\"User Logged In successfully.\",\"success\":true}",
+    "isFetching": "false",
+    "error": "true",
+    "_persist": "{\"version\":1,\"rehydrated\":true}"
+}
--- admin/dashboard/src/utilities/requestMethods.js
+++ admin/dashboard/src/utilities/requestMethods.js
@@ -3,7 +3,14 @@ import axios from "axios";
 const BASE_URI = "https://rymo-shop-api.onrender.com/api/v1";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.data.accessToken;
-// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YmQxMWVjYmRhMWEyN2E2NjRmNTFmZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMDQwMzE4NywiZXhwIjoxNzEwNDg5NTg3fQ.4LDOV8U1YGazGzuMZ23kZNb2qZmdiDdzFBT5A8xP_8Q";
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YmQxMWVjYmRhMWEyN2E2NjRmNTFmZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMDQwMzE4NywiZXhwIjoxNzEwNDg5NTg3fQ.4LDOV8U1YGazGzuMZ23kZNb2qZmdiDdzFBT5A8xP_8Q
+
+console
+  .log
+  // JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+  //   .data.accessToken
+  ();
 
 // Public Request
 export const publicRequest = axios.create({
